61e24805f9feab0d362e98d666ec12b8a75ce0dc
Jørgen Lund <jorgen.johannessen@student.nhh.no>
2024-09-30 10:14:51 +0200

    add python file

---
+++ LoopVsBuiltin.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+from math import sqrt
+
+NumLst = [1, 2, 3, 4, 5]
+
+def std_loops(x):
+    """
+    Compute standard deviation of x using loops.
+    Parameters
+    ----------
+    x: Sequence of numbers
+    Returns
+    -------
+    sd : float
+    Standard deviation of the list of numbers.
+    """
+    
+    
+    
+def std_builtin(x):
+    """
+    Compute standard deviation of x using the built-in functions sum()
+    and len().
+    Parameters
+    ----------
+    x: Sequence of numbers
+    Returns
+    -------
+    sd : float
+    Standard deviation of the list of numbers.
+    """
